55bb24b690a792c3975c663fa6312152bd62dbc9
bogachenko_da <denisoff26@yandex.ru>
2023-12-25 14:59:19 +0300

    1. Созданы модели Post и Reactions, создан шаблон страницы, добавлен flatpages about.

--- mmorpg/board/models.py
+++ mmorpg/board/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from django.urls import reverse
 
-# Create your models here.
+tanks = 'TNK'
+hils = 'HIL'
+dd = 'DD'
+merchants = 'MCH'
+guild_masters = 'GMA'
+quest_givers = 'QMA'
+blacksmiths = 'BLM'
+tanners = 'TAN'
+potion_makers = 'PMK'
+spell_masters = 'SMA'
+
+CATEGORY = [
+    (tanks, 'Танки'),
+    (hils, 'Хилы'),
+    (dd, 'ДД'),
+    (merchants, 'Торговцы'),
+    (guild_masters, 'Гилдмастеры'),
+    (quest_givers, 'Квестгиверы'),
+    (blacksmiths, 'Кузнецы'),
+    (tanners, 'Кожевники'),
+    (potion_makers, 'Зельевары'),
+    (spell_masters, 'Мастера заклинаний'),
+]
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = RichTextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=3, choices=CATEGORY, )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+    CATEGORY = CATEGORY
+
+
+class Reaction(models.Model):
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    accepted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
--- mmorpg/mmorpg/settings.py
+++ mmorpg/mmorpg/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'board',
+    'ckeditor',
+    'ckeditor_uploader',
+    'django_filters',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'mmorpg.urls'
@@ -54,7 +66,7 @@ ROOT_URLCONF = 'mmorpg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +133,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
